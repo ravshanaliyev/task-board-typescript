9960ba931e39ba92d724064dca8e2f6cc4944c6c
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-12-21 09:39:19 +0500

    theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Container, Heading } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Heading fontSize={{base: "4xl", sm: "5xl", md: "6xl"}} fontWeight={"bold"} textAlign={"center"} bgGradient={"linear(to-l, #7928CA, #FF0080)"} bgClip={"text"} mt={4}>
+        Welcome to Tick Tick Board
+      </Heading>
+      <Container maxWidth={"container.lg"} px={4} py={10}>
+
+      </Container>
+    </>
   );
 }
 
